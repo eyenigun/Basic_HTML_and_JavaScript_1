3b0095fddf2d04fca1c81a0b79f04dceb8c7bbcd
Erhan Yenigun <eyenigun@gmail.com>
2025-02-08 12:24:11 -0500

    Basic Javascript

---
+++ Basic_Javascript_2.js
@@ -0,0 +1,7 @@
+  function My_first_function() {
+            var String = "Kiss me I am Irish";
+            var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML = 
+		result ;
+       	 }
+
